4f3b48b71bf8ad929f51c21a159443c2b9284926
LavanyaLahari811 <lavanyalaharik2003@gmail.com>
2024-01-26 01:00:33 +0530

    intial setup

---
+++ index.js
@@ -0,0 +1,6 @@
+/**@type {HTMLCanvasElement}*/
+
+const canvas=document.querySelector("#canvas");
+const ctx=canvas.getContext("2d");
+CANVAS_HEIGHT=canvas.height=700;
+CANVAS_WIDTH=canvas.width=500;
